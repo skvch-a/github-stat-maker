597a66dffb7eb6761ef2e819332ef2bb65919ae9
Aleksey <skvch.al@gmail.com>
2024-10-17 22:19:29 +0500

    some fixes

--- stat_maker.py
+++ stat_maker.py
@@ -2,14 +2,13 @@ import asyncio
 
 from gql import Client
 from gql.transport.aiohttp import AIOHTTPTransport
+from gql.transport.exceptions import TransportQueryError
 
 from visualizer import draw_diagram
 from requests import REPOS_QUERY, BRANCHES_QUERY, COMMITS_QUERY, ORGANIZATION
 
-TOKEN = "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"
+TOKEN = "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"
 GRAPHQL_URL = "https://api.github.com/graphql"
-SEM = asyncio.Semaphore(5)
-
 
 def get_top_100(authors_by_emails):
     return sorted(authors_by_emails.items(), key=lambda x: x[1]["commits_count"], reverse=True)[:100]
@@ -26,47 +25,47 @@ async def update_authors(authors_by_emails, commits, lock):
                 authors_by_emails[email] = {"name": name, "commits_count": 1}
 
 
-async def try_get_response(query, variables):
+async def try_get_response(client, query, variables):
     try:
-        return await get_response(query, variables)
-    except Exception as e:
-        print('Ошибочка!', e)
-
-async def get_response(query, variables):
-    client = get_client()
-    response = await client.execute_async(query, variable_values=variables)
-    await client.close_async()
-    return response
+        return await client.execute_async(query, variable_values=variables)
+    except TransportQueryError:
+        print('Rate limit exceeded, try again later or change TOKEN')
+        exit()
 
 async def get_branch_names(repo_name):
     branches_names = []
     cursor = None
-
+    client = get_client()
     while True:
 
-        response = await try_get_response(BRANCHES_QUERY, {"repo": repo_name, "cursor": cursor, "owner": ORGANIZATION})
+        response = await try_get_response(client, BRANCHES_QUERY, {"repo": repo_name, "cursor": cursor, "owner": ORGANIZATION})
 
         branches = response["repository"]["refs"]
         branches_names.extend(branch["name"] for branch in branches["nodes"])
 
-        if not branches["pageInfo"]["hasNextPage"]:
+        if not branches["pageInfo"]["hasNextPage"] or True:
             break
 
         cursor = branches["pageInfo"]["endCursor"]
 
+    await client.close_async()
     return branches_names
 
 async def get_commits_for_branch(repo_name, branch_name, processed_commits, authors_by_emails, lock):
+    print(f'Ветка {branch_name} репозитория {repo_name}')
     all_commits = []
     is_over = False
     cursor = None
-
+    client = get_client()
     while True:
-        response = await try_get_response(COMMITS_QUERY,
+        response = await try_get_response(client, COMMITS_QUERY,
                                          {"repo": repo_name,
                                                    "branch": "refs/heads/" + branch_name,
                                                    "cursor": cursor,
                                                    "owner": ORGANIZATION})
+        if response is None:
+            print('ХУЕТА!!!!!!!!')
+            break
         commit_history = response["repository"]["ref"]["target"]["history"]
 
         for commit in commit_history["nodes"]:
@@ -82,12 +81,12 @@ async def get_commits_for_branch(repo_name, branch_name, processed_commits, auth
             break
 
         cursor = commit_history["pageInfo"]["endCursor"]
-
+    await client.close_async()
     await update_authors(authors_by_emails, all_commits, lock)
 
 
 async def process_repo(repo_name, authors_by_emails):
-    async with SEM:
+
         processed_commits = set()
         tasks = []
         lock = asyncio.Lock()
@@ -100,25 +99,24 @@ async def process_repo(repo_name, authors_by_emails):
 async def get_commiters():
     authors_by_emails = {}
     variables_for_repos_query = {"org": ORGANIZATION}
-
+    client = get_client()
     repo_count = 1
     has_next_page = True
 
 
     while has_next_page:
-
-        repos_data = await try_get_response(REPOS_QUERY, variables_for_repos_query)
-
         tasks = []
+        repos_data = await try_get_response(client, REPOS_QUERY, variables_for_repos_query)
 
         for repo in repos_data["organization"]["repositories"]["nodes"]:
             print(f'Обрабатывается репозиторий: {repo_count} - {repo["name"]}')
             repo_count += 1
             tasks.append(asyncio.create_task(process_repo(repo["name"], authors_by_emails)))
+        await asyncio.gather(*tasks)
 
         has_next_page = repos_data["organization"]["repositories"]["pageInfo"]["hasNextPage"]
         variables_for_repos_query["cursor"] = repos_data["organization"]["repositories"]["pageInfo"]["endCursor"]
-
+    await client.close_async()
     return authors_by_emails
 
 
