47fca1709e6b1972e664c4299571f10db27735b0
Aleksey <skvch.al@gmail.com>
2024-10-18 11:38:44 +0500

    Refactoring

--- stat_maker.py
+++ stat_maker.py
@@ -1,3 +1,4 @@
+import sys
 import asyncio
 
 from gql import Client
@@ -10,8 +11,6 @@ from visualizer import draw_diagram
 sem = asyncio.Semaphore(8)
 TOKEN = "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"
 GRAPHQL_URL = "https://api.github.com/graphql"
-GET_RESPONSE_LOCK_FOR_TRANSPORT_QUERY_ERROR = asyncio.Lock()
-
 
 async def update_authors(authors_by_emails, commits, lock):
     for commit in commits:
@@ -24,17 +23,11 @@ async def update_authors(authors_by_emails, commits, lock):
             else:
                 authors_by_emails[email] = {"name": name, "commits_count": 1}
 
-
 async def try_get_response(client, query, variables):
     try:
         return await client.execute_async(query, variable_values=variables)
     except TransportQueryError:
-        async with GET_RESPONSE_LOCK_FOR_TRANSPORT_QUERY_ERROR:
-            for task in asyncio.all_tasks():
-                if task is not asyncio.current_task():
-                    task.cancel()
-            print('Rate limit exceeded, try again later or change TOKEN')
-            exit()
+        sys.exit('Rate limit exceeded, try again later or change TOKEN')
     except Exception as e:
         print(f'Ошибка {e} на запросе {variables}, повтор запроса')
         return await try_get_response(client, query, variables)
@@ -63,42 +56,39 @@ async def get_branch_names(repo_name):
 
 
 async def get_commits_for_branch(repo_name, branch_name, processed_commits, authors_by_emails, lock):
-    try:
-        async with sem:
-            all_commits = []
-            is_over = False
-            cursor = None
-            client = get_client()
+    async with sem:
+        all_commits = []
+        is_over = False
+        cursor = None
+        client = get_client()
 
-            while True:
-                response = await try_get_response(client, COMMITS_QUERY,
-                                                  {"repo": repo_name,
-                                                   "branch": "refs/heads/" + branch_name,
-                                                   "cursor": cursor,
-                                                   "owner": ORGANIZATION})
-                if response is None:
+        while True:
+            response = await try_get_response(client, COMMITS_QUERY,
+                                              {"repo": repo_name,
+                                               "branch": "refs/heads/" + branch_name,
+                                               "cursor": cursor,
+                                               "owner": ORGANIZATION})
+            if response is None:
+                break
+
+            commits = response["repository"]["ref"]["target"]["history"]
+            for commit in commits["nodes"]:
+                if commit["oid"] in processed_commits:
+                    is_over = True
                     break
+                if commit["message"].startswith("Merge pull request #"):
+                    continue
+                processed_commits.add(commit["oid"])
+                all_commits.append(commit)
 
-                commits = response["repository"]["ref"]["target"]["history"]
-                for commit in commits["nodes"]:
-                    if commit["oid"] in processed_commits:
-                        is_over = True
-                        break
-                    if commit["message"].startswith("Merge pull request #"):
-                        continue
-                    processed_commits.add(commit["oid"])
-                    all_commits.append(commit)
+            if not commits["pageInfo"]["hasNextPage"] or is_over:
+                break
 
-                if not commits["pageInfo"]["hasNextPage"] or is_over:
-                    break
+            cursor = commits["pageInfo"]["endCursor"]
 
-                cursor = commits["pageInfo"]["endCursor"]
+        await client.close_async()
+        await update_authors(authors_by_emails, all_commits, lock)
 
-            await client.close_async()
-            await update_authors(authors_by_emails, all_commits, lock)
-
-    except asyncio.CancelledError:
-        return
 
 
 async def get_authors_by_emails():
