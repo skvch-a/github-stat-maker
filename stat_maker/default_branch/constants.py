2c8daa06abc55f06cf29deeb0d0ebf44f2f47b46
Aleksey <skvch.al@gmail.com>
2024-10-21 13:24:19 +0500

    refactoring

--- stat_maker/default_branch/constants.py
+++ stat_maker/default_branch/constants.py
@@ -1,7 +1,7 @@
 from gql import gql
 
 
-ORGANIZATION = 'godaddy'
+ORGANIZATION = 'twitter'
 TOKEN = "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"
 GRAPHQL_URL = "https://api.github.com/graphql"
 
@@ -23,29 +23,11 @@ REPOS_QUERY = gql(
     """
 )
 
-BRANCHES_QUERY = gql(
-    """
-    query($repo: String!, $cursor: String, $owner: String!) {
-      repository(name: $repo, owner: $owner) {
-        refs(first: 100, after: $cursor, refPrefix: "refs/heads/") {  
-          pageInfo {
-            endCursor
-            hasNextPage
-          }
-          nodes {
-            name  
-          }
-        }
-      }
-    }
-    """
-)
-
 COMMITS_QUERY = gql(
     """     
-    query($repo: String!, $branch: String!, $cursor: String, $owner: String!) {
+    query($repo: String!, $cursor: String, $owner: String!) {
       repository(name: $repo, owner: $owner) {
-        ref(qualifiedName: $branch) {
+        defaultBranchRef {
           target {
             ... on Commit {
               history(first: 100, after: $cursor) {  
